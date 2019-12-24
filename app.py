fb98535e0e6dcb63e72df8c68ae2efe4df8b07cd
heydar <heydar.javadi@gmail.com>
2019-12-24 12:26:43 +0330

    commit

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
 import requests
-from flask import request
+# from flask_mysqldb import MySQL
+
+from flaskext.mysql import MySQL
 
 
 app = Flask(__name__)
+
+
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = '1234'
+# app.config['MYSQL_DB'] = 'cloud'
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
+app.config['MYSQL_DATABASE_DB'] = 'cloud'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+# mysql = MySQL(app)
+
+mysql = MySQL()
+mysql.init_app(app)
 
 @app.route('/heartbeat', methods=['GET','POST'])
 def heartbeat():
@@ -20,10 +37,10 @@ def account_profile():
     email = request.values.get('email')
     passw = request.values.get('password')
     name = request.values.get('name')
-    phonen = request.values.get('phoneNo')
-    nationalc = request.values.get('nationalCode')
+    phoneN = request.values.get('phoneNo')
+    nationalC = request.values.get('nationalCode')
     address = request.values.get('address')
-    postalc = request.values.get('postalCode')
+    postalC = request.values.get('postalCode')
 
     print(email)
     print(passw)
@@ -37,8 +54,21 @@ def account_profile():
             data=params
             )
 
-    print(response)
+    cur = mysql.get_db().cursor()
+
+    if response.status_code == 200:
+    	print('Success!')
+    	command = "INSERT INTO users_profile(id, email,name,phoneNo,nationalCode,address,postalCode)"
+    	cur.execute(command+" VALUES (null, %s,%s,%d,%d,%s,%d)", (email, name,phoneN,nationalC,address,postalC))
+    else :
+    	print("fail")
+    	print(response.status_code)
+
+
     print(response.json()['message'])
+    
+    mysql.get_db.commit()
+    cur.close()
     
 
     return jsonify(response.json())
